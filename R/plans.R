85d2da5b78a0e474ce90842275d1259ca0c03bc5
mps9506 <mpschramm@gmail.com>
2021-08-04 15:48:28 -0500

    file caching

--- R/plans.R
+++ R/plans.R
@@ -3,6 +3,7 @@
 #' @param huc (character) 8-digit or higher HUC. required
 #' @param organization_id (character). optional
 #' @param summarize (logical)
+#' @param tidy (logical) \code{TRUE} (default) the function returns a tidied tibble. \code{FALSE} the function returns the raw JSON string.
 #' @param ... list of curl options passed to [crul::HttpClient()]
 #'
 #' @return tibble
@@ -10,6 +11,7 @@
 plans <- function(huc = NULL,
                   organization_id = NULL,
                   summarize = FALSE,
+                  tidy = TRUE,
                   ...) {
 
   summarize <- if(isTRUE(summarize)) {
@@ -25,7 +27,30 @@ plans <- function(huc = NULL,
   if(is_empty(args_present)) {
     stop("One of the following arguments must be provided: huc")
   }
+
+
+  ##setup file cache
+  plans_cache <- hoardr::hoard()
   path = "attains-public/api/plans"
-  content <- xGET(path, args, ...)
-  content <- fromJSON(content, simplifyVector = FALSE)
+  file <- file_key(path = path, arg_list = args)
+  plans_cache$cache_path_set(path = file)
+  plans_cache$mkdir()
+
+  ## check if current results have been cached
+  file_name <- file.path(plans_cache$cache_path_get(),
+                         "plans.json")
+  if(file.exists(file_name)) {
+    message(paste0("reading cached file from: ", file_name))
+    content <- readLines(file_name, warn = FALSE)
+  } else { ## download data
+    content <- xGET(path,
+                    args,
+                    file = file_name,
+                    ...)
+  }
+  if(!isTRUE(tidy)) { ## return raw data
+    return(content)
+  } else { ## return parsed data
+    content <- fromJSON(content, simplifyVector = FALSE)
+  }
 }
--- R/survey.R
+++ R/survey.R
@@ -3,6 +3,7 @@
 #' Downloads data about state statistical (probability) survey results.
 #' @param organization_id (character) - Filters the list to only those “belonging to” one of the specified organizations. Multiple values may be specified. required
 #' @param survey_year (character) Filters the list to the year the survey was performed. optional.
+#' @param tidy (logical) \code{TRUE} (default) the function returns a tidied tibble. \code{FALSE} the function returns the raw JSON string.
 #' @param ... list of curl options passed to [crul::HttpClient()]
 #'
 #' @return a tibble with multiple columns or a tibble with zero columns.
@@ -19,6 +20,7 @@
 
 surveys <- function(organization_id = NULL,
                     survey_year = NULL,
+                    tidy = TRUE,
                     ...) {
 
   coll <- checkmate::makeAssertCollection()
@@ -37,27 +39,48 @@ surveys <- function(organization_id = NULL,
   if(is_empty(args_present)) {
     stop("One of the following arguments must be provided: organization_id")
   }
-
+  ## setup file cache
+  surveys_cache <- hoardr::hoard()
   path = "attains-public/api/surveys"
-  content <- xGET(path, args, ...)
-  content <- fromJSON(content, simplifyVector = FALSE)
-  if(!rlang::is_empty(content$items)) {
-    content <- content$items %>%
-      tibble::enframe() %>%
-      select(!c(.data$name)) %>%
-      unnest_wider(.data$value) %>%
-      unnest_longer(.data$surveys) %>%
-      unnest_wider(.data$surveys) %>%
-      unnest_longer(.data$surveyWaterGroups) %>%
-      unnest_wider(.data$surveyWaterGroups) %>%
-      unnest_longer(.data$surveyWaterGroupUseParameters) %>%
-      unnest_wider(.data$surveyWaterGroupUseParameters) %>%
-      clean_names()
-  } else {
-    content <- tibble::tibble()
-  }
+  file <- file_key(path = path, arg_list = args)
+  surveys_cache$cache_path_set(path = file)
+  surveys_cache$mkdir()
 
-  return(content)
+  ## check if current results have been cached
+  file_name <- file.path(surveys_cache$cache_path_get(),
+                         "surveys.json")
+
+  if(file.exists(file_name)) {
+    message(paste0("reading cached file from: ", file_name))
+    content <- readLines(file_name, warn = FALSE)
+  } else { ## download data
+    content <- xGET(path,
+                    args,
+                    file = file_name,
+                    ...)
+  }
+  ## return raw JSON
+  if(!isTRUE(tidy)) return(content)
+  ## parse and tidy JSON
+  else {
+    content <- fromJSON(content, simplifyVector = FALSE)
+    if(!rlang::is_empty(content$items)) {
+      content <- content$items %>%
+        tibble::enframe() %>%
+        select(!c(.data$name)) %>%
+        unnest_wider(.data$value) %>%
+        unnest_longer(.data$surveys) %>%
+        unnest_wider(.data$surveys) %>%
+        unnest_longer(.data$surveyWaterGroups) %>%
+        unnest_wider(.data$surveyWaterGroups) %>%
+        unnest_longer(.data$surveyWaterGroupUseParameters) %>%
+        unnest_wider(.data$surveyWaterGroupUseParameters) %>%
+        clean_names()
+      } else {
+      content <- tibble::tibble()
+      }
+    return(content)
+    }
 }
 
 
