076e6135e73a9560ae2d70239b9a003b3ddce70a
mps9506 <mpschramm@gmail.com>
2021-08-12 10:28:14 -0500

    update file paths on vcr

--- temp.R
+++ temp.R
@@ -11,39 +11,75 @@ library(janitor)
 x1 <- assessments(organization_id = "SDDENR", probable_source = "GRAZING IN RIPARIAN OR SHORELINE ZONES", tidy = FALSE)
 x1 <- assessments(state_code = "OR", exclude_assessments = TRUE, tidy = FALSE)
 x1 <- assessments(state_code = "TX", tidy = TRUE)
-
-
-
-content <- content$items %>%
-  tibble::enframe() %>%
-  select(!c(.data$name)) %>%
-  unnest_wider(.data$value) %>%
-  unnest_longer(.data$documents) %>%
-  unnest_wider(.data$documents) %>%
-  unnest_longer(.data$documentTypes) %>%
-  unnest_wider(.data$documentTypes) %>%
-  janitor::clean_names()
+x1 <- domain_values(domain_name="UseName",context="TDECWR",tidy = FALSE)
+x1 <- huc12_summary(huc = "020700100204", tidy=FALSE)
+x1 <- plans(huc ="020700100103", summarize = FALSE, tidy = FALSE)
+x1 <- plans(huc ="020700100103", summarize = TRUE)
+x1 <- state_summary(organization_id = "TCEQMAIN", tidy = FALSE)
+x1 <- surveys(organization_id="SDDENR", tidy = FALSE)
+x1 <- surveys(organization_id = "TCEQMAIN", tidy = FALSE)
 
 
 x1 %>%
   enter_object("items") %>%
   gather_array() %>%
-  spread_all() %>%
-  select(-c(.data$array.index, .data$document.id)) %>%
+  spread_values(organizationIdentifier = jstring("organizationIdentifier"),
+                organizationName = jstring("organizationName"),
+                organizationTypeText = jstring("organizationTypeText")) %>%
+  select(-c(.data$document.id, .data$array.index)) %>%
+  enter_object("surveys") %>%
+  gather_array() %>%
+  spread_values(surveyStatusCode = jstring("surveyStatusCode"),
+                year = jnumber("year"),
+                surveyCommentText = jstring("surveyCommentText")) %>%
+  select(-c(.data$array.index)) %>%
+  enter_object("surveyWaterGroups") %>%
+  gather_array() %>%
+  spread_values(waterTypeGroupCode = jstring("waterTypeGroupCode"),
+                subPopulationCode = jstring("subPopulationCode"),
+                unitCode = jstring("unitCode"),
+                size = jnumber("size"),
+                siteNumber = jstring("siteNumber"),
+                surveyWaterGroupCommentText = jstring("surveyWaterGRoupCommentText")) %>%
+  select(-c(.data$array.index)) %>%
+  enter_object("surveyWaterGroupUseParameters") %>%
+  gather_array() %>%
+  spread_values(stressor = jstring("stressor"),
+                surveyUseCode = jstring("surveyUseCode"),
+                surveyCategoryCode = jstring("surveyCategoryCode"),
+                statistic = jstring("statistic"),
+                metricValue = jnumber("metricValue"),
+                confidenceLevel = jnumber("confidenceLevel"),
+                commentText = jstring("commentText")) %>%
+  select(-c(.data$array.index)) %>%
+  as_tibble() %>%
+  clean_names() -> content_surveys
+
+x1 %>%
+  enter_object("items") %>%
+  gather_array() %>%
+  spread_values(organizationIdentifier = jstring("organizationIdentifier"),
+                organizationName = jstring("organizationName"),
+                organizationTypeText = jstring("organizationTypeText")) %>%
+  select(-c(.data$document.id, .data$array.index)) %>%
+  enter_object("surveys") %>%
+  gather_array() %>%
+  spread_values(surveyStatusCode = jstring("surveyStatusCode"),
+                year = jnumber("year"),
+                surveyCommentText = jstring("surveyCommentText")) %>%
+  select(-c(.data$array.index)) %>%
   enter_object("documents") %>%
   gather_array() %>%
-  spread_all(recursive = TRUE) %>%
+  spread_values(agencyCode = jstring("agencyCode"),
+                documentFileType = jstring("documentFileType"),
+                documentFileName = jstring("documentFileName"),
+                documentDescription = jstring("documentDescription"),
+                documentComments = jstring("documentComments"),
+                documentURL = jstring("documentURL")) %>%
   select(-c(.data$array.index)) %>%
-  mutate(
-    documentTypes = map(.data$..JSON, ~{
-      .x[["documentTypes"]] %>% {
-        tibble(
-          assessmentTypeCode = map_chr(., "documentTypeCode")
-        )}
-    })) %>%
-  tibble::as_tibble() %>%
-  unnest(c(.data$documentTypes)) %>%
-  janitor::clean_names()-> content_docs
-
-
-x1 %>% gather_object() %>% filter(name == "count") %>% spread_values(count = jnumber()) %>% select(-c(document.id, name)) %>% as_tibble()
+  enter_object("documentTypes") %>%
+  gather_array() %>%
+  spread_all %>%
+  select(-c(.data$array.index)) %>%
+  as_tibble() %>%
+  clean_names() -> content_documents
--- tests/testthat/setup-rATTAINS.R
+++ tests/testthat/setup-rATTAINS.R
@@ -1,6 +1,11 @@
 library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
-  dir = vcr::vcr_test_path("fixtures"),
-  write_disk_path = vcr::vcr_test_path("files")
+  ## this creates an absolute path, probably need a relative path
+  ## dir = vcr::vcr_test_path("fixtures"),
+  dir = "../fixtures",
+  ## this creates an absolute path
+  ## write_disk_path = vcr::vcr_test_path("files")
+  ## probably want a relative path
+  write_disk_path = "../files"
 ))
 vcr::check_cassette_names()
