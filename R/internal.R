24f157f01ed602070805bb73bbe3f10d7f6bc287
mps9506 <mpschramm@gmail.com>
2026-06-29 16:47:36 -0400

    uncomment an important function

--- R/internal.R
+++ R/internal.R
@@ -1,4 +1,3 @@
-
 #' xGET
 #'
 #' Internal function for making http requests.
@@ -13,23 +12,26 @@
 #' @noRd
 xGET <- function(path, args = list(), file = NULL, ...) {
   url <- "https://api.epa.gov/attains"
-  cli <- crul::HttpClient$new(url,
-                              opts = list(...),
-                              headers = list("X-API-Key" = Sys.getenv("RATTAINS_TOKEN")))
+  cli <- crul::HttpClient$new(
+    url,
+    opts = list(...),
+    headers = list("X-API-Key" = Sys.getenv("RATTAINS_TOKEN"))
+  )
 
-  full_url <- cli$url_fetch(path = path,
-                            query = args)
+  full_url <- cli$url_fetch(path = path, query = args)
 
-  res <- cli$retry("GET",
-                   path = path,
-                   query = args,
-                   pause_base = 5,
-                   pause_cap = 60,
-                   pause_min = 5,
-                   times = 3,
-                   terminate_on = c(404),
-                   ...)
-  
+  res <- cli$retry(
+    "GET",
+    path = path,
+    query = args,
+    pause_base = 5,
+    pause_cap = 60,
+    pause_min = 5,
+    times = 3,
+    terminate_on = c(404),
+    ...
+  )
+
   errs(res)
 
   if (!is.null(res)) {
@@ -38,8 +40,6 @@ xGET <- function(path, args = list(), file = NULL, ...) {
     content <- NULL
     warning("Sorry, no data found", call. = FALSE)
   }
-
-
 
   return(content)
 }
@@ -55,19 +55,18 @@ xGET <- function(path, args = list(), file = NULL, ...) {
 #' @importFrom fauxpas find_error_class
 errs <- function(x) {
   if (x$status_code > 201) {
-
     tryCatch(
       {
         fun <- fauxpas::find_error_class(x$status_code)$new()
         fun$do(x)
       },
-      error = function(e) {NULL},
+      error = function(e) {
+        NULL
+      },
       finally = message(fun$mssg)
-
     )
   }
 }
-
 
 
 #' Check connectivity
@@ -77,42 +76,40 @@ errs <- function(x) {
 #' @noRd
 #' @importFrom curl nslookup
 check_connectivity <- function() {
-  tryCatch(expr = {
-    nslookup("attains.epa.gov")
-    return(TRUE)
+  tryCatch(
+    expr = {
+      nslookup("attains.epa.gov")
+      return(TRUE)
     },
-    error = function(e){
+    error = function(e) {
       message("No connection to <https://attains.epa.gov> available!")
     }
   )
 }
 
 
-#' #' Check API key
-#' #'
-#' #' Checks for API Key in current environment or provided by user.
-#' #' Should be included near the top of each function before the arg list
-#' #' is built.
-#' #' @return TRUE or error
-#' #' @keywords internal
-#' #' @noRd
-#' #'
-#' check_api_key <- function() {
-#'   tryCatch(expr = {
-#'     ## RATTAINS_TOKEN
-#'     ifelse(
-#'       nchar(Sys.getenv("RATTAINS_TOKEN")) > 0,
-#'       TRUE,
-#'       FALSE
-#'     )
-#'     return(TRUE)
+#' Check API key
 #'
-#'   },
-#'   error = function(e){
-#'     message("API Token from Data.gov using: https://api.data.gov/signup/")
-#'   }
+#' Checks for API Key in current environment or provided by user.
+#' Should be included near the top of each function before the arg list
+#' is built.
+#' @return TRUE or error
+#' @keywords internal
+#' @noRd
 #'
-#'   )
-#'
-#'
-#' }
+check_api_key <- function() {
+  tryCatch(
+    expr = {
+      ## RATTAINS_TOKEN
+      ifelse(
+        nchar(Sys.getenv("RATTAINS_TOKEN")) > 0,
+        TRUE,
+        FALSE
+      )
+      return(TRUE)
+    },
+    error = function(e) {
+      message("API Token from Data.gov using: https://api.data.gov/signup/")
+    }
+  )
+}
